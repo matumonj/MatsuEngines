308c9d208dbfe2a37b375f1a323a3f840ae3b939
matumonj <74886038+matumonj@users.noreply.github.com>
2022-08-13 23:42:14 +0900

    チュートリアル用スプライト　明日用

--- MyGame/TutorialSprite.cpp
+++ MyGame/TutorialSprite.cpp
@@ -1,1 +1,16 @@
 #include "TutorialSprite.h"
+void TutorialSprite::Initialize()
+{
+	Sprite::LoadTexture(170, L"Resources/Tuto");
+	Task[0]=Sprite::Create(170, { 10,10 });
+}
+
+void TutorialSprite::Update()
+{
+
+}
+
+void TutorialSprite::Draw()
+{
+
+}
--- MyGame/TutorialSprite.h
+++ MyGame/TutorialSprite.h
@@ -1,5 +1,24 @@
 #pragma once
+#include"Sprite.h"
 class TutorialSprite
 {
+private:
+	static const int TaskNum = 5;
+	Sprite* Task[TaskNum];
+	enum Tasks{
+		HELLO,
+		WALK,
+		ATTAK,
+		SETTING,
+		CLEAR
+	};
+	Tasks task = HELLO;
+
+public:
+	void Initialize();
+
+	void Update();
+
+	void Draw();
 };
 
